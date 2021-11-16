91854846520ff1aa7e4516189bf54377bd252d25
Steven Quintana <steve.e.quint@gmail.com>
2021-11-16 08:13:47 -0800

    Fill out Applicant private vars and public methods

--- src/com/jofer/javabackend/user/Applicant.java
+++ src/com/jofer/javabackend/user/Applicant.java
@@ -1,5 +1,30 @@
 package com.jofer.javabackend.user;
 
+import java.util.ArrayList;
+import com.jofer.javabackend.job.Job;
+
 public class Applicant extends User {
-    // Create unique attributes for applicant.
+    // Applicant private class variables.
+    ArrayList<Job> appliedJobs;
+
+    // Default Constructor
+    public Applicant() {
+        appliedJobs = new ArrayList<>();
+    }
+
+    // Setter methods
+    public void setAppliedJobs( ArrayList<Job> jobs ) {
+        appliedJobs = jobs;
+    }
+
+    // Methods to add a new job to applied jobs list.
+    public void addAppliedJob( Job job ) {
+        appliedJobs.add( job );
+    }
+
+    // Getter methods
+    public ArrayList<Job> getAppliedJobs() {
+        return appliedJobs;
+    }
+
 }
