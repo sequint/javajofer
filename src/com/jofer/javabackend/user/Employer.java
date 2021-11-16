c4db625b224619f8795e5c24f32e1fdfa11a08fb
Steven Quintana <steve.e.quint@gmail.com>
2021-11-16 10:39:01 -0800

    Delete job list property

--- src/com/jofer/javabackend/user/Employer.java
+++ src/com/jofer/javabackend/user/Employer.java
@@ -6,33 +6,20 @@ import com.jofer.javabackend.job.Job;
 public class Employer extends User {
     // Employer private class variables.
     String company;
-    ArrayList<Job> postedJobs;
 
     // Default Constructor
     public Employer() {
         company = "";
-        postedJobs = new ArrayList<>();
     }
 
     // Setter methods
     public void setCompany( String comp ) {
         company = comp;
     }
-    public void setPostedJobs( ArrayList<Job> jobs ) {
-        postedJobs = jobs;
-    }
-
-    // Add a new job to the current posted jobs list.
-    public void addPostedJob( Job job ) {
-        postedJobs.add( job );
-    }
 
     // Getter methods
     public String getCompany() {
         return company;
-    }
-    public ArrayList<Job> getPostedJobs() {
-        return postedJobs;
     }
 
 }
