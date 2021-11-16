8eee2d51f8ff21010a4bb5137994074d91cc1c43
Steven Quintana <steve.e.quint@gmail.com>
2021-11-15 16:52:06 -0800

    Create properties, constructor, and setter/getter methods for User

--- src/com/jofer/javabackend/user/User.java
+++ src/com/jofer/javabackend/user/User.java
@@ -1,4 +1,30 @@
 package com.jofer.javabackend.user;
 
 public class User {
+    // User class private variables
+    String firstName,
+            lastName,
+            username,
+            avatar;
+
+    // Default Constructor
+    public User() {
+        firstName = "";
+        lastName = "";
+        username = "";
+        avatar = "";
+    }
+
+    // Setter methods
+    public void setFirstName( String first ) { firstName = first; }
+    public void setLastName( String last ) { lastName = last; }
+    public void setUsername( String userNm ) { username = userNm; }
+    public void setAvatar( String avt ) { avatar = avt; }
+
+    // Getter methods
+    public String getFirstName() { return firstName; }
+    public String getLastName() { return lastName; }
+    public String getUsername() { return username; }
+    public String getAvatar() { return avatar; }
+
 }
