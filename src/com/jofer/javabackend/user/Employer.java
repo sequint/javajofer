75d4d40ec08c57ba00feb812632b85325f5107c4
Steven Quintana <steve.e.quint@gmail.com>
2021-11-15 17:10:04 -0800

    Create Employer class property with getter and setter functions

--- src/com/jofer/javabackend/user/Employer.java
+++ src/com/jofer/javabackend/user/Employer.java
@@ -1,5 +1,21 @@
 package com.jofer.javabackend.user;
 
+import java.util.Vector;
+
 public class Employer extends User {
     // Create unique attributes for Employer class.
+    String company;
+    // Create a vector for posted jobs.
+
+    // Default Constructor
+    public Employer() {
+        company = "";
+    }
+
+    // Setter methods
+    public void setCompany( String comp ) { company = comp; }
+
+    // Getter methods
+    public String getCompany() { return company; }
+
 }
