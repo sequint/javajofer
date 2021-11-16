3f055f73fff54c912d1179f8c08326390ffea65d
Steven Quintana <steve.e.quint@gmail.com>
2021-11-16 07:45:58 -0800

    Import job class to Employer and create a list of jobs in the class

--- src/com/jofer/javabackend/user/Employer.java
+++ src/com/jofer/javabackend/user/Employer.java
@@ -1,15 +1,18 @@
 package com.jofer.javabackend.user;
 
-import java.util.Vector;
+import java.util.ArrayList;
+import java.util.HashMap;
+import com.jofer.javabackend.job.Job;
 
 public class Employer extends User {
     // Create unique attributes for Employer class.
     String company;
-    // Create a vector for posted jobs.
+    ArrayList<Job> postedJobs;
 
     // Default Constructor
     public Employer() {
         company = "";
+        postedJobs = new ArrayList<Job>();
     }
 
     // Setter methods
