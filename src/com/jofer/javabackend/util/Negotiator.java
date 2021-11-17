be21880280d36ecd7c2b729decb555c3f0cc0115
Steven Quintana <steve.e.quint@gmail.com>
2021-11-17 08:49:52 -0800

    Create initial offer set method and a handler for counter offers

--- src/com/jofer/javabackend/util/Negotiator.java
+++ src/com/jofer/javabackend/util/Negotiator.java
@@ -13,6 +13,7 @@ public class Negotiator {
     private boolean employerAcceptedOffer;
     private boolean applicantDeclinedOffer;
     private boolean employerDeclinedOffer;
+    private double initialOffer;
     private double applicantsCounter;
     private double employersCounter;
     private double finalSalary;
@@ -28,9 +29,46 @@ public class Negotiator {
         employerAcceptedOffer = false;
         applicantDeclinedOffer = false;
         employerDeclinedOffer = false;
+        initialOffer = 0.00;
         applicantsCounter = 0.00;
         employersCounter = 0.00;
         finalSalary = 0.00;
     }
+
+    // Setter methods for job and applicant objects.
+    public void setJob( Job job ) {
+        this.job = job;
+    }
+    public void setApplicant( Applicant applicant ) {
+        this.applicant = applicant;
+    }
+
+    // Handle an initial offer.
+    public void setInitialOffer( double initialOffer ) {
+        this.initialOffer = initialOffer;
+        this.jobOffered = true;
+    }
+
+    // Handle counter offer.
+    public void handleCounterOffer( double counter, String user ) {
+
+        // If the user is an applicant set applicant variables, otherwise set employer variables.
+        if ( user.equals("applicant") ) {
+            this.applicantsCounter = counter;
+            this.applicantCountered = true;
+            this.employerCountered = false;
+        }
+        else if ( user.equals("employer") ) {
+            this.employersCounter = counter;
+            this.employerCountered = true;
+            this.applicantCountered = false;
+        }
+        else {
+            System.out.println("Invalid user");
+        }
+
+    }
+
+    // Handle offer accepted.
 
 }
